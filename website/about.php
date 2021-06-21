162b85f507d5208ac2c377e9b34e81766b8162c4
MaxBouricius <55273830+MaxBouricius@users.noreply.github.com>
2021-06-21 14:56:32 +0200

    About page begining

---
+++ website/about.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/product-slider.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com/%22%3E">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+    <script type="module" src="js/script.js" defer></script>
+    <title>About us!</title>
+</head>
+<body>
+<div class="nav-parent">
+        <nav>          
+            <div class="icon">
+                <i class="fa fa-bars"></i>
+            </div>
+        </nav>
+        <ul class="nav-items">
+            <img src="./foto/logo.png" alt="">
+            <div class="nav-links">
+                <a href="#"><li>Home</li></a>
+                <a href="#"><li>Cart</li></a>
+                <a href="#"><li>About</li></a>
+                <a href="#"><li>Contact</li></a>
+            </div>
+        </ul>
+</div>
+
+<div class="introduction">
+    <h1>Hi! We're Duckyz</h1>
+    <p>We are a new and </p>
+    <img src="./foto/logo.png" alt="">
+</div>
+</body>
+</html>
