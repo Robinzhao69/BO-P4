e4c7d9e5b9eeb7bd4ecb2406a1d34870ebc64d34
MaxBouricius <55273830+MaxBouricius@users.noreply.github.com>
2021-06-28 01:58:13 +0200

    we

--- website/contactPage.php
+++ website/contactPage.php
@@ -2,9 +2,31 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/product-slider.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com/%22%3E">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+    <script type="module" src="js/script.js" defer></script>
     <title>Contact form</title>
 </head>
 <body>
+<div class="nav-parent">
+        <nav>          
+            <div class="icon">
+                <i class="fa fa-bars"></i>
+            </div>
+        </nav>
+        <ul class="nav-items">
+            <img src="./foto/logo.png" alt="">
+            <div class="nav-links">
+                <a href="#"><li>Home</li></a>
+                <a href="http://31716.hosts1.ma-cloud.nl/website/wordpress/"><li>Shop</li></a>
+                <a href="#"><li>About</li></a>
+                <a href="#"><li>Contact</li></a>
+            </div>
+        </ul>
+</div>
     <main>
     <p>SEND E-MAIL</p>
     <form class="contact-form" action="contactform.php" method="post"></form>
